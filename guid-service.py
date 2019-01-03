b19446298a385c077b64a77095921f42c9e5af7d
buzzdavidson <buzzdavidson@me.com>
2019-01-03 13:01:12 -0800

    Return hex representation of GUID (ie, no dashes); add JSON representation; allow user to specify count of values to retrieve; update service catalog

--- guid-service.py
+++ guid-service.py
@@ -1,23 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 import sys
 import optparse
 import time
 import uuid
+import json
 
 app = Flask(__name__)
 
 start = int(round(time.time()))
 
 def get_uuid_string():
-    return str(uuid.uuid4())
+    return uuid.uuid4().hex
 
 @app.route("/api/v1/guid/text")
-def get_plaintext_guid():
+def get_plaintext_guid_text():
     return get_uuid_string();
+
+@app.route("/api/v1/guid")
+def get_plaintext_guid_json():
+    c = request.args.get("count")
+    if c is None:
+        c = "1"
+    count=int(c)
+    values = []
+    for x in xrange(count):
+        values.append(get_uuid_string())
+        
+    return json.dumps(values);
 
 @app.route("/")
 def get_default_page():
-    return "<h1>Catalog</h1><p>This service builds UUIDs!</p><p>You probably want <a href='/api/v1/guid/text'>/api/v1/guid/text</a></p>"
+    return "<h1>GUID Service</h1><p>UUID Generation Service</p><ul><li><a href='/api/v1/guid/text'>/api/v1/guid/text (plaintext)</a></li><li><a href='/api/v1/guid'>/api/v1/guid (JSON) - NOTE: accepts count parameter</a></li></ul>"
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage="python simpleapp.py -p ")
